d3adb389887ac893a4f077a9c381e01dc948b6cc
Louis White <louis.white1500@gmail.com>
2021-07-25 18:00:12 +0800

    feat: add no promo component for when no promos are announced

---
+++ src/components/home/nopromo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NoPromo: React.FC = () => {
+  return (
+    <div className='no-promo'>
+      <p>No promos announced.</p>
+      <p>Navigate to the <Link to='/division-rivals'>Divison Rivals</Link> tab to view the weekly Divison Rivals countdown.</p>  
+    </div>
+  );
+}
+
+export default NoPromo;
--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,46 +1,12 @@
 import React from "react";
-import { Container, Row } from "reactstrap";
-import Countdown from "../shared/countdown";
-import * as countdownHelper from "../../helpers/countdown-helper";
+import { Container } from "reactstrap";
+import NoPromo from "../home/nopromo";
 
 const Home: React.FC = () =>  {
   return (
     <div>
       <Container>
-        <Row className='justify-content-center'>
-          <h3 className='text-center'>Team of the Season Countdowns</h3>
-        </Row>
-        <Row className='justify-content-center'>
-          <p className='text-center'>NOTE: Dates are speculative and based on previous years TOTS</p>
-        </Row>
-        <br />
-        <Countdown 
-          countdownTo={countdownHelper.getCommunityDate()} 
-          title='Community + EFL' />
-        <br />
-        <Countdown 
-          countdownTo={countdownHelper.getPremierLeagueDate()} 
-          title='PL + Saudi' />
-        <br />
-        <Countdown 
-          countdownTo={countdownHelper.getLaLigaDate()} 
-          title='La Liga + Super Lig' />
-        <br />
-        <Countdown 
-          countdownTo={countdownHelper.getBundesDate()} 
-          title='Bundes + Liga NOS' />
-        <br />
-        <Countdown 
-          countdownTo={countdownHelper.getSerieADate()} 
-          title='Serie A + Eredivisie &amp; CSL' />
-        <br />
-        <Countdown 
-          countdownTo={countdownHelper.getLigueOneDate()} 
-          title='Ligue One + MLS &amp; LATAM' />
-        <br />
-        <Countdown 
-          countdownTo={countdownHelper.getUltimateDate()} 
-          title='Ultimate + ROW' />
+        <NoPromo />
       </Container>
     </div>
   )
