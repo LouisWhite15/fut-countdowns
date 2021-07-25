6c329e2a57814f52d5855e070b1a0bfb53accecf
Louis White <louis.white1500@gmail.com>
2021-07-25 20:42:40 +0800

    feat: added footer with a link to the github repo for the community to raise new promos

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,7 +6,12 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = () => {
 
   return (
-    <footer> </footer>
+    <footer className='footer'>
+      <div>
+        <p>&copy; Copyright Louis White 2021</p>
+        <p>If there is a new promo you would like to see added, raise an issue on GitHub <a href='https://github.com/LouisWhite15/fut-countdowns' rel='noreferrer'>here</a></p>
+      </div>
+    </footer>
   )
 }
 
