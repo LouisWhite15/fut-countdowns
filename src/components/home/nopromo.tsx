bbfce0ca108b6f3b441f1f617f088820fd0a0541
Louis White <louis.white1500@gmail.com>
2021-07-25 18:01:57 +0800

    fix: typo in no promo component

--- src/components/home/nopromo.tsx
+++ src/components/home/nopromo.tsx
@@ -5,7 +5,7 @@ const NoPromo: React.FC = () => {
   return (
     <div className='no-promo'>
       <p>No promos announced.</p>
-      <p>Navigate to the <Link to='/division-rivals'>Divison Rivals</Link> tab to view the weekly Divison Rivals countdown.</p>  
+      <p>Navigate to the <Link to='/division-rivals'>Division Rivals</Link> tab to view the weekly Divison Rivals countdown.</p>  
     </div>
   );
 }
