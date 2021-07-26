15b08d6352b3c4367c83d48a45c086a18532b54c
Louis White <louis.white1500@gmail.com>
2021-07-26 11:42:09 +0800

    feat: updated footer content

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className='footer'>
       <div>
-        <p>&copy; Copyright Louis White 2021</p>
+        <p>Author: Louis White 2021</p>
         <p>If there is a new promo you would like to see added, raise an issue on GitHub <a href='https://github.com/LouisWhite15/fut-countdowns' rel='noreferrer'>here</a></p>
       </div>
     </footer>
