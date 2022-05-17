91b92ccaef8ea20d6ee0fb6b1e470ef905a62ce8
Louis White <louis.white1500@gmail.com>
2022-05-17 21:06:14 +0800

    feat: removed information from the footer and removed pointless countdown helper methods

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,8 +8,6 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className='footer'>
       <div>
-        <p>Author: Louis White 2021</p>
-        <p>If there is a new promo you would like to see added, raise an issue on GitHub <a href='https://github.com/LouisWhite15/fut-countdowns' target='_blank' rel='noreferrer'>here</a></p>
       </div>
     </footer>
   )
--- src/helpers/countdown-helper.tsx
+++ src/helpers/countdown-helper.tsx
@@ -1,34 +1,6 @@
 import { DateTime } from "luxon";
 import parser from "cron-parser";
 
-export const getCommunityDate = () => {
-  return DateTime.utc(2021, 4, 23, 17, 0, 0);
-}
-
-export const getPremierLeagueDate = () => {
-  return DateTime.utc(2021, 4, 30, 17, 0, 0);
-}
-
-export const getLaLigaDate = () => {
-  return DateTime.utc(2021, 5, 7, 17, 0, 0);
-}
-
-export const getBundesDate = () => {
-  return DateTime.utc(2021, 5, 14, 17, 0, 0);
-}
-
-export const getSerieADate = () => {
-  return DateTime.utc(2021, 5, 21, 17, 0, 0);
-}
-
-export const getLigueOneDate = () => {
-  return DateTime.utc(2021, 5, 28, 17, 0, 0);
-}
-
-export const getUltimateDate = () => {
-  return DateTime.utc(2021, 6, 4, 17, 0, 0);
-}
-
 export const getNextDivisionRivalsDate = () => {
   
   var options = {
